24c9a835c90df7280a138fe3bb25f0727f225fa4
asmallick-gif <asmallick@fit.edu.ph>
2026-07-06 17:21:16 +0800

    modified:   loginlogout w features/account.php

--- loginlogout w features/account.php
+++ loginlogout w features/account.php
@@ -10,42 +10,7 @@ if(!isset($_SESSION["login"]))
 <!DOCTYPE html>
 <html>
     <body>
-    <h2>Welcome!</h2>
-
-<table>
-    <tr>
-        <td>First Name:</td>
-        <td><?php echo $_SESSION["fname"]; ?></td>
-    </tr>
-    <tr>
-        <td>Middle Name:</td>
-        <td><?php echo $_SESSION["mname"]; ?></td>
-    </tr>
-    <tr>
-        <td>Last Name:</td>
-        <td><?php echo $_SESSION["lname"]; ?></td>
-    </tr>
-    <tr>
-        <td>Address:</td>
-        <td><?php echo $_SESSION["address"]; ?></td>
-    </tr>
-    <tr>
-        <td>Phone Number:</td>
-        <td><?php echo $_SESSION["phonenum"]; ?></td>
-    </tr>
-    <tr>
-        <td>Email:</td>
-        <td><?php echo $_SESSION["email"]; ?></td>
-    </tr>
-    <tr>
-        <td>Username:</td>
-        <td><?php echo $_SESSION["username"]; ?></td>
-    </tr>
-    <tr>
-        <td>Password:</td>
-        <td><?php echo $_SESSION["password"]; ?></td>
-    </tr>
-</table>
+    <h2>Welcome, <?php echo $_SESSION["fname"]; ?> ! </h2>
 
 <br><br>
 <a href="logout.php">Logout</a>
